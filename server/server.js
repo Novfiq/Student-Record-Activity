0d74f5ed4aa39232fe2628af3323d3cce89a210a
Novfiq <qifvon2005@gmail.com>
2026-05-01 17:00:38 +0530

    fixed render server port

--- server/server.js
+++ server/server.js
@@ -59,4 +59,8 @@ app.use(
 
 /* ===== SERVER ===== */
 
-app.listen(process.env.PORT || 5000);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
