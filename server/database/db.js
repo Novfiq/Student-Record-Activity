61e884de8d31b93e8bfcd4530627edfb034359b3
Novfiq <qifvon2005@gmail.com>
2026-05-02 07:17:03 +0530

    fixed mysql port

--- server/database/db.js
+++ server/database/db.js
@@ -1,22 +1,27 @@
-require("dotenv").config();
-
 const mysql = require("mysql2");
 
-console.log("DB HOST:", process.env.DB_HOST);
-
 const db = mysql.createConnection({
+
   host: process.env.DB_HOST,
+
   user: process.env.DB_USER,
+
   password: process.env.DB_PASSWORD,
+
   database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT)
+
+  port: process.env.DB_PORT
 });
 
 db.connect((err) => {
+
   if (err) {
-    console.log("DB ERROR:", err);
+
+    console.log(err);
+
   } else {
-    console.log("MySQL Connected");
+
+    console.log("Database Connected");
   }
 });
 
